52eaca0486e591c68839c19d67ac8db8123305a8
Deepak7200 <mail.deepaksehrawat@gmail.com>
2026-04-03 19:15:58 +0530

    about us and no page found pages added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import './App.css'
 
 import { Route,Routes } from 'react-router-dom'
 
+import AboutUs from './Pages/AboutUs'
 import HomePage from './Pages/HomePage'
+import NotFound from './Pages/NotFound'
 
 
 function App() { 
@@ -11,6 +13,8 @@ function App() {
     <>
       <Routes>
          <Route path="/" element={<HomePage/>}> Home </Route>
+         <Route path="/about" element={<AboutUs/>}> Home </Route>
+         <Route path="*" element={<NotFound/>}> Home </Route>
       </Routes>
     </>
   )
